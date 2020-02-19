b81ee0489f05961aeb15eaa73d9f62868074771b
yyr <315629555@qq.com>
2020-02-20 00:15:37 +0800

    更新upload.bat脚本,自动分批上传到github，且失败自动重试

--- upload.bat
+++ upload.bat
@@ -1,5 +1,24 @@
-for /l %%i in (35,1,39) do (
-    git add %%i*
-    git commit -m "save: %%i*"
-    git push origin master
+@echo off
+set /a beg = 371
+set /a end = 499
+set /a i = %beg%
+
+:forData
+if %i% GTR %end% (
+  goto over
 )
+git add %i%*.zip
+git commit -m "save: %i%*.zip"
+
+set /a j = 1
+:forRetry
+echo "%i%*: Try to push...%j%"
+git push origin master
+if %ERRORLEVEL% == 0 (
+    set /a i = %i% + 1
+    goto forData
+)
+set /a j = %j% + 1
+goto forRetry
+
+:over
